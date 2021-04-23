df18d17f9a2ada99301e01ae65ac3ab69b6c0f49
sebastianschneitz <sebschneitz@gmail.com>
2021-04-23 19:01:21 +0200

    added script files

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re"),WA.sendChatMessage('Hello world', 'Mr Robot')})();
+(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re"),WA.onEnterZone('cards_against_humanity', () => {
+                                                                                                             WA.openTab('https://picturecards.online/static/index.html');
+                                                                                                         }),
+                                                                                                         WA.onEnterZone('Codenames', () => {
+                                                                                                             WA.openTab('https://codenames.game/');
+                                                                                                         }),
+                                                                                                         WA.onEnterZone('scribbl', () => {
+                                                                                                             WA.openTab('https://skribbl.io/');
+                                                                                                         }),
+                                                                                                         WA.onEnterZone('gartic', () => {
+                                                                                                             WA.openTab('https://garticphone.com/de');
+                                                                                                         })})();
 //# sourceMappingURL=script.js.map

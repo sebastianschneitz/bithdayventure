f70d1386d6104d8682555322cad9c413cf3d1ac7
sebastianschneitz <sebschneitz@gmail.com>
2021-04-23 18:51:30 +0200

    some changes

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
+(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re"),WA.sendChatMessage('Hello world', 'Mr Robot')})();
 //# sourceMappingURL=script.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.openCoWebSite('https://workadventu.re');
-
+WA.onEnterZone('cards_against_humanity', () => {
+    WA.openTab('https://picturecards.online/static/index.html');
+});
+WA.onEnterZone('Codenames', () => {
+    WA.openTab('https://codenames.game/');
+});
+WA.onEnterZone('scribbl', () => {
+    WA.openTab('https://skribbl.io/');
+});
+WA.onEnterZone('gartic', () => {
+    WA.openTab('https://garticphone.com/de');
+});
